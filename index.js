f7e8840285b3aa4bf6343b32c89cd846b06e9b09
Taelor Lambert <taeloralysse@gmail.com>
2023-03-10 15:33:50 -0500

    cats

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+const copyOfCats = [...cats]
+function destructivelyAppendCat(name) {
+const destructivelyAppendCat = [...cats, name]
+cats.push("Ralph")}
+function destructivelyPrependCat(name) {
+    const destructivelyPrependCat = [name, ...cats]
+    cats.unshift("Bob")}
+    function destructivelyRemoveLastCat(name) {
+        cats.pop(-1)
+    }
+    function destructivelyRemoveFirstCat(name) {
+        cats.shift()
+    }
+    function appendCat(name) {
+        const copyOfCats = [...cats]
+        return appendCat = [...cats, "Broom"]
+    }
+    function prependCat(name) {
+        return prependCat = ["Arnold", ...cats]
+    }
+    function removeLastCat(name) {
+        return removeLastCat = cats.slice(0, 2)
+    }
+    function removeFirstCat(name) {
+        return removeFirstCat = cats.slice(1)
+    }
